9864131db6d2309c60978fd9430fa1e4b884aa24
robinsongz <mrrobinsung@gmail.com>
2018-11-19 14:30:48 -0800

    fixed crit dmg formula

--- src/Components/PopUps.js
+++ src/Components/PopUps.js
@@ -27,7 +27,7 @@ class DamageFormula extends Component {
                 <br />
         {<b>Crit Damage</b>}: [{<b>{this.props.atk}</b>} ATK + {<b>{this.props.critAtk}</b>} Crit ATK ] <FontAwesomeIcon icon="times" /> [ 1 + {<b>{this.props.atkIncrease}%</b>} Atk Increase / 100 ] <FontAwesomeIcon icon="times" /> [ 1 + (2.5*({<b>{this.props.bossAtk}% </b>} Boss Atk / 100)) ] <FontAwesomeIcon icon="times" /> [ 1 + {<b>{this.props.playerAtk}% </b>} Player Atk / 100 ] <FontAwesomeIcon icon="times" /> <FontAwesomeIcon icon="arrow-right" />
                 <br />
-                <FontAwesomeIcon icon="arrow-right" /> [ 1 + ({<b>{this.props.dmgIncrease}% </b>}Dmg Increase + {<b>{this.props.critDmg}%</b>} Crit Dmg) / 100 ] <FontAwesomeIcon icon="times" /> [ {<b>{this.props.skillDmg}%</b>} Skill Dmg ] <FontAwesomeIcon icon="times" /> [ {<b>{this.props.skillHit}</b>} Skill Hit ]
+                <FontAwesomeIcon icon="arrow-right" /> [ 1 + ({<b>{this.props.dmgIncrease}% </b>}Dmg Increase + (20 + {<b>{this.props.critDmg}%</b>}) Crit Dmg) / 100 ] <FontAwesomeIcon icon="times" /> [ {<b>{this.props.skillDmg}%</b>} Skill Dmg ] <FontAwesomeIcon icon="times" /> [ {<b>{this.props.skillHit}</b>} Skill Hit ]
                 <br />
         {<b>Average Damage</b>}: ((1 - {<b>{this.props.critRate}%</b>} Crit Rate / 100) * {<b>Non-Crit Damage</b>} ) + (({<b>{this.props.critRate}%</b>} Crit Rate / 100) * {<b>Crit Damage</b>} )
             </div>
